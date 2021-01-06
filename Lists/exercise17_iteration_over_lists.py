485aaca733527fadcda34fdc2f11c0e7dc1e6c92
Iga <iga.miekus@gmail.com>
2021-01-06 21:10:19 +0100

    list exercises

---
+++ Lists/exercise17_iteration_over_lists.py
@@ -0,0 +1,8 @@
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+# Create one large combned string:
+result = ''
+for s in sounds:
+    result += s.upper()
+
+print(result)
